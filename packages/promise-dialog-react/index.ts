531b7d00b8e7b4941f1c112b9acca5925bd47348
HangMine <95629358@qq.com>
2023-10-21 18:49:38 +0800

    🎨 react:增加doc项目

--- packages/promise-dialog-react/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/Dialog';
-export * from './src/modal-controller';
-export * from './src/types';
-export * from './src/use-dialog';
---
+++ packages/promise-dialog-react/src/index.ts
@@ -0,0 +1,4 @@
+export * from './Dialog';
+export * from './modal-controller';
+export * from './types';
+export * from './use-dialog';
